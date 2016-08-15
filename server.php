4131b86c56d0ca1ac8a3c88a112587fdea53abcc
Ethan DeSon <edeson@ucsd.edu>
2016-08-15 16:57:35 -0700

    Added UI

--- server.php
+++ server.php
@@ -1,9 +1,15 @@
 <html>
+<head>
+  <link rel="stylesheet" type="text/css" href="index.css">
+</head>
 <body>
   <title>Sonos Controller</title>
+  <div id="title"><div>Sonos Controller</div></div>
   <iframe id="sonosAction" style="display:none" src=""></iframe>
-  <div onclick="sendCommand('play')">Play!</div>
-  <div onclick="sendCommand('pause')">Pause!</div>
+  <div id="buttonContainer">
+    <div class="commandButton" onclick="sendCommand('play')">Play!</div>
+    <div class="commandButton" onclick="sendCommand('pause')">Pause!</div>
+  </div>
   <script>
     function sendCommand(cmd) {
       document.getElementById("sonosAction").src = "http://localhost/sonosAction.php?cmd="+cmd;
