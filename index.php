b474803c96425d1d4f9d5952142ab0311b43dddd
Ethan DeSon <edeson@ucsd.edu>
2016-08-15 17:36:55 -0700

    Changed server.php index.php

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+  <title>Sonos Controller</title>
+  <div id="title"><div>Sonos Controller</div></div>
+  <iframe id="sonosAction" style="display:none" src=""></iframe>
+  <div id="buttonContainer">
+    <div class="commandButton" onclick="sendCommand('play')">Play!</div>
+    <div class="commandButton" onclick="sendCommand('pause')">Pause!</div>
+  </div>
+  <script>
+    function sendCommand(cmd) {
+      document.getElementById("sonosAction").src = "/sonosAction.php?cmd="+cmd;
+    }
+  </script>
+</body>
+</html>
